196d8ee6361132a0e485fe6db853f7f267e26537
ibrahim <aydinibrahim1692@gmail.com>
2022-12-19 18:10:25 +0300

    Removed unused imports

--- app/src/main/java/com/example/hotelapi/ui/activity/NewsActivity.kt
+++ app/src/main/java/com/example/hotelapi/ui/activity/NewsActivity.kt
@@ -2,11 +2,9 @@ package com.example.hotelapi.ui.activity
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
 import androidx.navigation.findNavController
 import androidx.navigation.ui.setupWithNavController
-import com.example.hotelapi.R
 import com.example.hotelapi.databinding.ActivityNewsBinding
 import com.example.hotelapi.db.ArticleDatabase
 import com.example.hotelapi.repository.NewsRepository
